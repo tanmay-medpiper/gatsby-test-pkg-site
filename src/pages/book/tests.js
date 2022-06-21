2656ea31a892648bec9cf12368c628b72684c41e
Manish S <manish@medpiper.com>
2022-06-21 15:30:31 +0530

    Rescruption code, and add links

---
+++ src/pages/book/tests.js
@@ -0,0 +1,12 @@
+import React from "react"
+import SearchDiv from "../../components/SearchDiv/SearchDiv"
+
+const book_test = () => {
+  return (
+    <>
+      <SearchDiv />
+    </>
+  )
+}
+
+export default book_test
